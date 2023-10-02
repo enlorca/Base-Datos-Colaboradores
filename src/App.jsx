16499584702ab283867b6a15f4ea592669da1c49
Jose Corredor <jos3.corredor@gmail.com>
2023-10-01 22:47:07 -0500

    :tada: Se crea componente listado, utilizado un estado desde el componente principal que controla los usuarios registrados y que se muestran en la tabla

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {ListaColaboradores} from './ListaColaboradores'
+import Listado from './Components/Listado/Listado';
+import { useState } from 'react';
+import Formulario from './Components/Formulario/Formulario';
 function App() {
+  const [Colaboradores, setColaboradores] = useState(ListaColaboradores)
   return (
-    <>
-
-    </>
+    <main className='container'>
+    <h1 className='text-center'>Lista de colaboradores</h1>
+    <Listado ListaColaboradores={Colaboradores}/>
+    <Formulario/>
+    </main>
   )
 }
 
---
+++ src/Components/Listado/Listado.jsx
@@ -0,0 +1,35 @@
+import './Listado.css'
+import Table from 'react-bootstrap/Table';
+
+const Listado = ({ListaColaboradores}) => {
+  return (
+    <div className='table'>
+    <Table striped>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Correo</th>
+          <th>Edad</th>
+          <th>Cargo</th>
+          <th>Telefono</th>
+        </tr>
+      </thead>
+      <tbody>
+            {ListaColaboradores.map((colaborador) => { 
+                return (
+                    <tr key={colaborador.id}>
+                        <td>{colaborador.nombre}</td>
+                        <td>{colaborador.correo}</td>
+                        <td>{colaborador.edad}</td>
+                        <td>{colaborador.cargo}</td>
+                        <td>{colaborador.telefono}</td>
+                    </tr>   
+                )
+            })}
+      </tbody>
+    </Table>
+    </div>
+  )
+}
+
+export default Listado
